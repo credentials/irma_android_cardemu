credentials/irma_android_cardemu

commit b11b6cd075ed38a669bf157b43f27361a4a69bbd

Should fix the "se_feedback_text not found" issue

src/org/irmacard/cardemu/selfenrol/Passport.java
@@ -504,6 +504,7 @@ public class Passport extends Activity {
 
     private void prepareErrowScreen() {
         setContentView(R.layout.enroll_activity_error);
+        screen = SCREEN_ERROR;
 
         Resources r = getResources();
         switch (screen) {

commit e494de261d7cd2e2f8efa8bd6b74bcb78effd959

Fix small bug in error screen (regression from b11b6cd)

src/org/irmacard/cardemu/selfenrol/Passport.java
@@ -534,7 +534,6 @@ public class Passport extends Activity {
 
     private void prepareErrowScreen() {
         setContentView(R.layout.enroll_activity_error);
-        screen = SCREEN_ERROR;
 
         Resources r = getResources();
         switch (screen) {
@@ -547,6 +546,8 @@ public class Passport extends Activity {
             case SCREEN_START:
                 ((TextView)findViewById(R.id.step_text)).setTextColor(r.getColor(R.color.irmared));
         }
+
+        screen = SCREEN_ERROR;
     }
 
     private void setBacFieldWatcher() {