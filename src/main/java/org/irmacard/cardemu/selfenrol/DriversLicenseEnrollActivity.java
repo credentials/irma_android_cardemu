e712f05bd05e2a6f1e543e0f14d4689c7fe40e8a
Fabian <f.vandenbroek@cs.ru.nl>
2016-01-19 13:11:29 +0100

    WIP on eDL self-enrolment

--- src/main/java/org/irmacard/cardemu/selfenrol/DriversLicenseEnrollActivity.java
+++ src/main/java/org/irmacard/cardemu/selfenrol/DriversLicenseEnrollActivity.java
@@ -14,8 +14,11 @@ import com.google.gson.reflect.TypeToken;
 
 import net.sf.scuba.smartcards.CardService;
 import net.sf.scuba.smartcards.CardServiceException;
+import net.sf.scuba.smartcards.CommandAPDU;
+import net.sf.scuba.smartcards.ISO7816;
 import net.sf.scuba.smartcards.ProtocolCommands;
 import net.sf.scuba.smartcards.ProtocolResponses;
+import net.sf.scuba.smartcards.ResponseAPDU;
 
 import org.acra.ACRA;
 import org.irmacard.cardemu.BuildConfig;
@@ -39,23 +42,34 @@ import org.irmacard.mno.common.PassportVerificationResultMessage;
 import org.irmacard.mno.common.RequestFinishIssuanceMessage;
 import org.irmacard.mno.common.RequestStartIssuanceMessage;
 import org.jmrtd.BACKeySpec;
+import org.jmrtd.DESedeSecureMessagingWrapper;
 import org.jmrtd.PassportService;
 import org.jmrtd.Util;
 import org.jmrtd.lds.DG14File;
 import org.jmrtd.lds.DG15File;
 import org.jmrtd.lds.DG1File;
+import org.jmrtd.lds.MRZInfo;
 import org.jmrtd.lds.SODFile;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.Security;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Random;
 
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 
 public class DriversLicenseEnrollActivity extends AbstractNFCEnrollActivity {
@@ -76,9 +90,28 @@ public class DriversLicenseEnrollActivity extends AbstractNFCEnrollActivity {
     // Date stuff
     protected SimpleDateFormat bacDateFormat = new SimpleDateFormat("yyMMdd", Locale.US);
 
+    /** Copied from JMRTD.. to be removed when functional */
+    private transient Cipher cipher;
+    private transient Mac mac;
+    private static final IvParameterSpec ZERO_IV_PARAM_SPEC = new IvParameterSpec(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+    protected Random random;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /* TODO remove this when ported back to JMRTD*/
+        random = new SecureRandom();
+        try {
+            cipher = Cipher.getInstance("DESede/CBC/NoPadding");
+            mac = Mac.getInstance("ISO9797Alg3Mac", new org.spongycastle.jce.provider.BouncyCastleProvider());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, e.getStackTrace().toString());
+        } catch (NoSuchPaddingException e) {
+            Log.e(TAG, e.getStackTrace().toString());
+        }
+        /* until here */
 
         setNfcScreen(SCREEN_PASSPORT);
 
@@ -133,7 +166,7 @@ public class DriversLicenseEnrollActivity extends AbstractNFCEnrollActivity {
         try {
             service.open();
             PassportService passportService = new PassportService(service);
-            passportService.sendSelectApplet(false);
+            //passportService.sendSelectApplet(false);
 
             if (passportMsg == null) {
                 passportMsg = new PassportDataMessage(message.getSessionToken(), imsi, message.getNonce());
@@ -197,35 +230,265 @@ public class DriversLicenseEnrollActivity extends AbstractNFCEnrollActivity {
 
     }
 
-    /**
-     */
-    public synchronized void doBAP(PassportService ps, String Kdoc) throws CardServiceException {
+    /***********************************************************************************************
+     * FROM HERE SHOULD BE MOVED TO JMRTD                                                         *
+     **********************************************************************************************/
+    public synchronized void doBAP(PassportService ps) throws CardServiceException {
         String mrz = settings.getString("mrz", "");
         if (mrz != null && mrz.length()>0) {
             //String to byte array
             //redo computeKeySeedForBAC
+            Log.e(TAG, "de mrz is: "+ mrz);
 
 //TODO
-            /*
             try {
-                byte[] keySeed = computeKeySeedForBAC(bacKey);
+                String kdoc = mrz.substring(1, mrz.length() - 1);
+                Log.e(TAG, "de mrz waar we mee rekenenen is eigenlijk: "+ kdoc);
+                byte[] keySeed = computeKeySeedForBAP2(kdoc);
+                //byte[] keySeed = computeKeySeedForBAP(kdoc);
+                //byte[] keySeed = getBytes(mrz);
                 SecretKey kEnc = Util.deriveKey(keySeed, Util.ENC_MODE);
                 SecretKey kMac = Util.deriveKey(keySeed, Util.MAC_MODE);
-
+                Log.e(TAG, "kEnc = "+ toHexString(kEnc.getEncoded()));
+                Log.e(TAG, "kMac = "+ toHexString(kMac.getEncoded()));
                 try {
-                    ps.doBAC(kEnc, kMac);
+                    doBAC(ps, kEnc, kMac);
                 } catch (CardServiceException cse) {
                     Log.e(TAG,"BAC failed");
+                    Log.e(TAG, cse.getMessage().toString());
                     throw cse;
                 }
             } catch (GeneralSecurityException gse) {
+                Log.e(TAG,gse.getStackTrace().toString());
                 throw new CardServiceException(gse.toString());
             }
         } else {
             //TODO error no valid mrz string
-            */
+
         }
     }
+
+
+/* included and changed to test BAP algo, should be removed and possibly ported back to JMRTD*/
+    public synchronized void doBAC(PassportService ps, SecretKey kEnc, SecretKey kMac) throws CardServiceException, GeneralSecurityException {
+        byte[] rndICC = ps.sendGetChallenge();
+        Log.e(TAG, "gotten challenge: " + toHexString(rndICC));
+        byte[] rndIFD = new byte[8];
+        random.nextBytes(rndIFD);
+        byte[] kIFD = new byte[16];
+        random.nextBytes(kIFD);
+        byte[] response = sendMutualAuth(rndIFD, rndICC, kIFD, kEnc, kMac, ps);
+        Log.e(TAG,"sent the mutualAuth, gotten response: " + toHexString(response));
+        byte[] kICC = new byte[16];
+        System.arraycopy(response, 16, kICC, 0, 16);
+        byte[] keySeed = new byte[16];
+        for (int i = 0; i < 16; i++) {
+            keySeed[i] = (byte) ((kIFD[i] & 0xFF) ^ (kICC[i] & 0xFF));
+        }
+        SecretKey ksEnc = Util.deriveKey(keySeed, Util.ENC_MODE);
+        SecretKey ksMac = Util.deriveKey(keySeed, Util.MAC_MODE);
+        long ssc = Util.computeSendSequenceCounter(rndICC, rndIFD);
+
+    }
+
+    /**
+     * Sends an <code>EXTERNAL AUTHENTICATE</code> command to the passport.
+     * This is part of BAC.
+     * The resulting byte array has length 32 and contains <code>rndICC</code>
+     * (first 8 bytes), <code>rndIFD</code> (next 8 bytes), their key material "
+     * <code>kICC</code>" (last 16 bytes).
+     *
+     * @param rndIFD our challenge
+     * @param rndICC their challenge
+     * @param kIFD our key material
+     * @param kEnc the static encryption key
+     * @param kMac the static mac key
+     *
+     * @return a byte array of length 32 containing the response that was sent
+     *         by the passport, decrypted (using <code>kEnc</code>) and verified
+     *         (using <code>kMac</code>)
+     *
+     * @throws CardServiceException on tranceive error
+     */
+    public synchronized byte[] sendMutualAuth(byte[] rndIFD, byte[] rndICC, byte[] kIFD, SecretKey kEnc, SecretKey kMac, PassportService ps) throws CardServiceException {
+        Log.e(TAG, "started mutual auth with:");
+        Log.e(TAG, "RND.IFD = " + toHexString(rndIFD));
+        Log.e(TAG, "RND.ICC = " + toHexString(rndICC));
+        Log.e(TAG, "K.IFD = " + toHexString(kIFD));
+
+        try {
+            if (rndIFD == null || rndIFD.length != 8) { throw new IllegalArgumentException("rndIFD wrong length"); }
+            if (rndICC == null || rndICC.length != 8) { rndICC = new byte[8]; Log.e(TAG,"serious problem");}
+            if (kIFD == null || kIFD.length != 16) { throw new IllegalArgumentException("kIFD wrong length"); }
+            if (kEnc == null) { throw new IllegalArgumentException("kEnc == null"); }
+            if (kMac == null) { throw new IllegalArgumentException("kMac == null"); }
+
+            cipher.init(Cipher.ENCRYPT_MODE, kEnc, ZERO_IV_PARAM_SPEC);
+            Log.e(TAG, "initiatlized cipher");
+			/*
+			 * cipher.update(rndIFD); cipher.update(rndICC); cipher.update(kIFD); //
+			 * This doesn't work, apparently we need to create plaintext array. //
+			 * Probably has something to do with ZERO_IV_PARAM_SPEC.
+			 */
+            byte[] plaintext = new byte[32];
+            System.arraycopy(rndIFD, 0, plaintext, 0, 8);
+            System.arraycopy(rndICC, 0, plaintext, 8, 8);
+            System.arraycopy(kIFD, 0, plaintext, 16, 16);
+            byte[] ciphertext = cipher.doFinal(plaintext);
+            if (ciphertext.length != 32) {
+                throw new IllegalStateException("Cryptogram wrong length " + ciphertext.length);
+            }
+            Log.e(TAG, "Computed response");
+            mac.init(kMac);
+            Log.e(TAG, "initialized MAC");
+            byte[] paddedinput = Util.padWithMRZ(ciphertext);
+            Log.e(TAG,"ciphertext: " + toHexString(ciphertext));
+            Log.e(TAG,"padded ciphertext: " + toHexString(paddedinput));
+            byte[] mactext = mac.doFinal(paddedinput);
+            if (mactext.length != 8) {
+                Log.e(TAG, "apparantly MAC is of wrong length");
+                throw new IllegalStateException("MAC wrong length");
+            }
+
+            byte p1 = (byte) 0x00;
+            byte p2 = (byte) 0x00;
+
+            byte[] data = new byte[32 + 8];
+            System.arraycopy(ciphertext, 0, data, 0, 32);
+            System.arraycopy(mactext, 0, data, 32, 8);
+            Log.e(TAG,"cmd_data = " + toHexString(data));
+            int le = 40; /* 40 means max ne is 40 (0x28). */
+            CommandAPDU capdu = new CommandAPDU(ISO7816.CLA_ISO7816, ISO7816.INS_EXTERNAL_AUTHENTICATE, p1, p2, data, le);
+            Log.e(TAG,"about to send command APDU: " + toHexString(capdu.getBytes()));
+            ResponseAPDU rapdu = ps.transmit(capdu);
+
+            byte[] rapduBytes = rapdu.getBytes();
+            short sw = (short)rapdu.getSW();
+            Log.e(TAG,"received response " + toHexString(rapduBytes));
+            if (rapduBytes == null) {
+                throw new CardServiceException("Mutual authentication failed", sw);
+            }
+
+			/* Some MRTDs apparently don't support 40 here, try again with 0. See R2-p1_v2_sIII_0035 (and other issues). */
+            if (sw != ISO7816.SW_NO_ERROR) {
+                Log.e(TAG,"we get here???" + sw);
+                le = 0; /* 0 means ne is max 256 (0xFF). */
+                capdu = new CommandAPDU(ISO7816.CLA_ISO7816, ISO7816.INS_EXTERNAL_AUTHENTICATE, p1, p2, data, le);
+                Log.e(TAG,"about to send command APDU: " + toHexString(capdu.getBytes()));
+                rapdu = ps.transmit(capdu);
+                rapduBytes = rapdu.getBytes();
+                Log.e(TAG,"received response " + toHexString(rapduBytes));
+                sw = (short)rapdu.getSW();
+            }
+
+            if (rapduBytes.length != 42) {
+                throw new CardServiceException("Mutual authentication failed: expected length: 40 + 2, actual length: " + rapduBytes.length, sw);
+            }
+
+			/*
+			 * byte[] eICC = new byte[32]; System.arraycopy(rapdu, 0, eICC, 0, 32);
+			 *
+			 * byte[] mICC = new byte[8]; System.arraycopy(rapdu, 32, mICC, 0, 8);
+			 */
+
+			/* Decrypt the response. */
+            cipher.init(Cipher.DECRYPT_MODE, kEnc, ZERO_IV_PARAM_SPEC);
+            byte[] result = cipher.doFinal(rapduBytes, 0, rapduBytes.length - 8 - 2);
+            if (result.length != 32) {
+                throw new IllegalStateException("Cryptogram wrong length " + result.length);
+            }
+            return result;
+        } catch (GeneralSecurityException gse) {
+            throw new CardServiceException(gse.toString());
+        }
+    }
+
+
+    private static byte[] computeKeySeedForBAP(String kdoc) throws GeneralSecurityException {
+        if (kdoc == null || kdoc.length() < 6) {
+            throw new IllegalArgumentException("Wrong document key for drivers license, found " + kdoc);
+        }
+
+        byte[] keySeed = computeKeySeed(kdoc, "SHA-1", true);
+
+        return keySeed;
+    }
+
+    private static byte[] computeKeySeedForBAP2(String kdoc) throws GeneralSecurityException {
+        if (kdoc == null || kdoc.length() < 6) {
+            throw new IllegalArgumentException("Wrong document key for drivers license, found " + kdoc);
+        }
+
+        //byte[] keySeed = computeKeySeed(kdoc, "SHA-1", true);
+        MessageDigest shaDigest = MessageDigest.getInstance("SHA-1");
+        shaDigest.update(getBytes(kdoc));
+        byte[] hash = shaDigest.digest();
+
+        //truncate
+        byte[] keySeed = new byte[16];
+        System.arraycopy(hash, 0, keySeed, 0, 16);
+        return keySeed;
+    }
+
+    public static byte[] computeKeySeed(String kdoc, String digestAlg, boolean doTruncate) throws GeneralSecurityException {
+
+		/* Check digits... */
+        byte[] documentNumberCheckDigit = { (byte) MRZInfo.checkDigit(kdoc) };
+
+        MessageDigest shaDigest = MessageDigest.getInstance(digestAlg);
+
+        shaDigest.update(getBytes(kdoc));
+        shaDigest.update(documentNumberCheckDigit);
+
+        byte[] hash = shaDigest.digest();
+
+        if (doTruncate) {
+			/* FIXME: truncate to 16 byte only for BAC with 3DES. Also for PACE and/or AES? -- MO */
+            byte[] keySeed = new byte[16];
+            System.arraycopy(hash, 0, keySeed, 0, 16);
+            return keySeed;
+        } else {
+            return hash;
+        }
+    }
+
+    //helper function from JMRTD Util. Unchanged.
+    private static byte[] getBytes(String str) {
+        byte[] bytes = str.getBytes();
+        try {
+            bytes = str.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException use) {
+			/* NOTE: unlikely. */
+            Log.e(TAG,"Exception: " + use.getMessage());
+        }
+        return bytes;
+    }
+    /***********************************************************************************************
+     * UNTIL HERE SHOULD BE MOVED TO JMRTD                                                         *
+     **********************************************************************************************/
+
+    /*
+    * debug code for hex string from SCUBA
+    * can pribably be removed after we have working code
+    * */
+    public static String byteToHexString(byte b) {
+        int n = b & 0x000000FF;
+        String result = (n < 0x00000010 ? "0" : "") + Integer.toHexString(n);
+        return result.toUpperCase();
+    }
+    public static String bytesToHexString(byte[] text, int offset, int length, int numRow) {
+        if(text == null) return "NULL";
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            if(i != 0 && i % numRow == 0) result.append("\n");
+            result.append(byteToHexString(text[offset + i]));
+        }
+        return result.toString();
+    }
+    public static String toHexString(byte[] text) {
+        return bytesToHexString(text,0,text.length, 1000);
+    }
+    /* end debug code*/
 
     /**
      * Reads the datagroups 1, 14 and 15, and the SOD file and requests an active authentication from an e-passport
@@ -258,13 +521,15 @@ public class DriversLicenseEnrollActivity extends AbstractNFCEnrollActivity {
                 // reason there is no need to let it crash the app.)
                 try {
                     //TODO
-                    //doBAP(ps, );//ps.doBAC(getBAPKey());
+                    doBAP(ps);//ps.doBAC(getBAPKey());
                     Log.i(TAG, "doing BAP");
-                } catch (/*CardServiceException | */IllegalStateException e) {
+                } catch (CardServiceException | IllegalStateException e) {
                     bacError = true;
                     Log.e(TAG, "doing BAP failed");
                     return null;
                 }
+
+                Log.e(TAG,"TADA!!! BAP SUCCEEDED!!!");
 
                 Exception ex = null;
                 try {
