8dac81caa016cd42be966add5db525b47b658c87
Sietse Ringers <mail@sietseringers.net>
2016-09-20 09:56:20 +0200

    Fix double screenshot preference when switching apps

--- src/main/java/org/irmacard/cardemu/preferences/ScreenshotPreferenceFragment.java
+++ src/main/java/org/irmacard/cardemu/preferences/ScreenshotPreferenceFragment.java
@@ -1,14 +1,14 @@
 package org.irmacard.cardemu.preferences;
 
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 import org.irmacard.cardemu.R;
 
 public class ScreenshotPreferenceFragment extends PreferenceFragment {
 	@Override
-	public void onResume() {
-		super.onResume();
-
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences_screenshots);
 	}
 }
